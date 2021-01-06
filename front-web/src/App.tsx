5d6f00379da2c733e06d8a5a2be61a5cb98a6b6b
ldavip <ldavi@outlook.com>
2021-01-06 20:33:49 -0300

    Create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './Counter';
+import  Hello from './Hello';
+
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
