5dafcfa685d98c34f22785dd47885c48f9c3860a
ldavip <ldavi@outlook.com>
2021-01-07 20:38:15 -0300

    List products from API

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,39 +1,26 @@
+import { useEffect, useState } from "react";
+import { fetchProducts } from "../api";
+import ProductsList from "./ProductsList";
 import StepsHeader from "./StepsHeader";
+import { Product } from "./types";
 
 function Orders() {
+
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetchProducts()
+      .then(response => setProducts(response.data))
+      .catch(error => console.error(error))
+  }, [])
+
   return (
+
     <div className="orders-container">
-      <StepsHeader />  
+      <StepsHeader />
+      <ProductsList products={products} />
     </div>
   )
 }
 
 export default Orders;
-
-
-// const handleSelectProduct = (product: Product) => {
-//   const isAlreadySelected = selectedProducts.some(item => item.id === product.id);
-
-//   if (isAlreadySelected) {
-//     const selected = selectedProducts.filter(item => item.id !== product.id);
-//     setSelectedProducts(selected);
-//   } else {
-//     setSelectedProducts(previous => [...previous, product]);
-//   }
-// }
-
-// const handleSubmit = () => {
-//   const productsIds = selectedProducts.map(({ id }) => ({ id }));
-//   const payload = {
-//     ...orderLocation!,
-//     products: productsIds
-//   }
-
-//   saveOrder(payload).then(() => {
-//     toast.error('Pedido enviado com sucesso!');
-//     setSelectedProducts([]);
-//   })
-//     .catch(() => {
-//       toast.warning('Erro ao enviar pedido');
-//     })
-// }
