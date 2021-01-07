de00c62df6618f866099235cc2431876d53cdf30
ldavip <ldavi@outlook.com>
2021-01-07 07:48:20 -0300

    Add routes and StepHeader

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../Footer';
 import { ReactComponent as MainImage } from './main.svg';
 import './styles.css';
@@ -12,16 +13,16 @@ function Home() {
                     <div className="home-actions">
                         <h1 className="home-title">
                             Faça seu pedido <br />
-                        que entregamos <br />
-                        pra você!!!
-                    </h1>
+                            que entregamos <br />
+                            pra você!!!
+                        </h1>
                         <h3 className="home-subtitle">
                             Escolha o seu pedido e em poucos minutos <br />
-                        levaremos na sua porta
-                    </h3>
-                        <a href="orders" className="home-btn-order">
+                            levaremos na sua porta
+                        </h3>
+                        <Link to="/orders" className="home-btn-order">
                             FAZER PEDIDO
-                    </a>
+                        </Link>
                     </div>
                     <div className="home-image">
                         <MainImage />
